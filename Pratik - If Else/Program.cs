4a8e95e9c668fb67e06da5f9b6ae3a5516c463f0
ekinnn35 <ekinn.9193@gmail.com>
2024-12-18 16:22:59 +0300

    Add project files.

---
+++ Pratik - If Else/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.WriteLine("Lütfen bir sayı giriniz");
+
+int sayi = int.Parse(Console.ReadLine());
+
+if(sayi > 10)
+{
+    Console.WriteLine("Bu sayı 6'dan büyüktür.");
+    if(sayi % 2 == 0 )
+    {
+        Console.WriteLine("Girilen sayı çifttir.");
+    }
+    else
+    {
+        Console.WriteLine("Girilen sayı tektir.");
+    }
+}
+else if ( sayi == 6)
+{
+    Console.WriteLine("Bu sayı 6'ya eşittir.");
+}
+else
+{
+    Console.WriteLine("Bu sayı 6'dan küçüktür.");
+    if (sayi %2 == 0 )
+    {
+        Console.WriteLine("Girilen sayı çifttir.");
+    }
+    else
+    {
+        Console.WriteLine("Girilen sayı tektir.");
+    }
+}
+
+Console.ReadLine();
